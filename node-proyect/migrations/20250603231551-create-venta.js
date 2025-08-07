30282294e55a5448920104a88d1159dad2d63968
Gonzalo Toledo <g.toledo@itecriocuarto.org.ar>
2025-08-07 19:51:30 -0300

    Agregar referencias y restricciones de integridad a las columnas usuarioId y productoId en la tabla Venta

--- node-proyect/migrations/20250603231551-create-venta.js
+++ node-proyect/migrations/20250603231551-create-venta.js
@@ -10,10 +10,22 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       usuarioId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Usuarios', // nombre de la tabla (¡ojo! debe coincidir con la migración de Usuarios)
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
       },
       productoId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Productos',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
       },
       cantidad: {
         type: Sequelize.INTEGER
@@ -33,6 +45,7 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Venta');
